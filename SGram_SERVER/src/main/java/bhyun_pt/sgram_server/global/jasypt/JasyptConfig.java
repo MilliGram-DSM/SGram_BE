b35336bc8df26c46b245e47b855d85b8511c0835
나병현 <nbhyun0329@gmail.com>
2024-08-22 19:30:45 +0900

    FEAT :: application.yml 암호화

---
+++ SGram_SERVER/src/main/java/bhyun_pt/sgram_server/global/jasypt/JasyptConfig.java
@@ -0,0 +1,31 @@
+package bhyun_pt.sgram_server.global.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Bean("jasyptEncryptorAES")
+    public StringEncryptor jasyptEncryptorAES() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword("password");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64"); //인코딩 방식
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
