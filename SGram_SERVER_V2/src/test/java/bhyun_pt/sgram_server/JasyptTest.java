5ad0d749d58ff5fb8b24cbfbae64617f69131a2d
나병현 <nbhyun0329@gmail.com>
2024-09-01 17:43:44 +0900

    FIX :: yml 파일 암호화

---
+++ SGram_SERVER_V2/src/test/java/bhyun_pt/sgram_server/JasyptTest.java
@@ -0,0 +1,25 @@
+package bhyun_pt.sgram_server;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+public class JasyptTest {
+    @Test
+    public void jasyptTest() {
+
+        //given
+        String password = "";
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword(password);
+        jasypt.setAlgorithm("PBEWITHMD5ANDDES");
+
+        //when
+        String encryptedText = jasypt.encrypt("");
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+        System.out.println(encryptedText);
+        System.out.println(decryptedText);
+
+
+    }
+}
--- SGram_SERVER_V2/src/test/java/bhyun_pt/sgram_server/SGramServerApplicationTests.java
+++ SGram_SERVER_V2/src/test/java/bhyun_pt/sgram_server/SGramServerApplicationTests.java
@@ -3,11 +3,13 @@ package bhyun_pt.sgram_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SGramServerApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 
 }
